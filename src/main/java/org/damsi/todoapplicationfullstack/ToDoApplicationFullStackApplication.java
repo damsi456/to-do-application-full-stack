777b84a82e8ca5f6ca0efe934f07e2220f0ff253
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-03-26 19:05:32 +0530

    Initial Commit

---
+++ src/main/java/org/damsi/todoapplicationfullstack/ToDoApplicationFullStackApplication.java
@@ -0,0 +1,13 @@
+package org.damsi.todoapplicationfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplicationFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApplicationFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/damsi/todoapplicationfullstack/ToDoApplicationFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.damsi.todoapplicationfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
